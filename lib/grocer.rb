747eb1fc7205fa5ad0a3842435fb9f696cb70383
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-12-04 23:53:46 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,7 @@ def find_item_by_name_in_collection(name, collection)
   counter = 0
 
   while counter < collection.length
-    if
+    if collection[counter] == name
     end
     counter += 1
   end
