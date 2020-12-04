c60bc3ec0bab6f5059f6be1da2792f815fa6d5f3
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-12-04 23:57:10 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -11,7 +11,9 @@ def find_item_by_name_in_collection(name, collection)
 
   collection.each do |value|
     value.each do |key, val|
-      binding.pry
+      if value[:item] == name
+        return value[:item]
+      end
     end
   end
 
