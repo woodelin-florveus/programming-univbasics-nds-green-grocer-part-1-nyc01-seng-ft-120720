ee8d50023fb9ffce699a26671f3e91c7ab16c6a8
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-12-05 00:20:23 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -19,6 +19,7 @@ def consolidate_cart(cart)
   while counter < cart.length
     cart_item = find_item_by_name_in_collection(cart[counter][:item], new_arr)
     if cart_item != nil
+      binding.pry
       cart_item[:count] += 1
     else
       cart_item = {
