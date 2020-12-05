7e65371744141b751a388486de384aaa00d31a4d
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-12-05 00:07:40 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -14,4 +14,10 @@ end
 def consolidate_cart(cart)
   # return a new array of hashes
   new_arr = []
+  counter = 0
+
+  while counter < cart.length
+    cart_item = find_item_by_name_in_collection(cart[counter][:item])
+    counter += 1
+  end
 end
